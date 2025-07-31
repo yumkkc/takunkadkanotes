5eb0792fe93be91d7b68bee564e393a22592665b
Saiyam Chettri <saiyum303@gmail.com>
2025-08-01 00:37:10 +0530

    Initial commit

---
+++ build-site.el
@@ -0,0 +1,24 @@
+(require 'ox-publish)
+
+;; Definte the publishing project
+(setq org-html-validation-link nil
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
+      org-html-head "<link rel\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+
+(setq org-publish-project-alist
+      (list
+       (list "takunkkadka_blog"
+             :recursive t
+             :base-directory "./content"
+             :publishing-directory "./public"
+             :publishing-function 'org-html-publish-to-html
+             :with-author nil
+             :with-creator t
+             :section-number nil
+             :time-stamp-file nil)))
+
+;; generate the site output
+(org-publish-all t)
+
+(message "Build Completed")
